30f2686dd54d5fbe7c7629f57bd3c70e4809da03
vuhongson23 <140721299+vuhongson23@users.noreply.github.com>
2025-11-11 13:42:13 +0700

    refactor code

--- src/app/(main)/friend/search/page.tsx
+++ src/app/(main)/friend/search/page.tsx
@@ -1,6 +1,32 @@
+import UserCard from "@/app/(main)/friend/search/user-card";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Search } from "lucide-react";
+
 const SearchFriend = () => {
   return (
-    <div className="p-5 bg-white rounded-lg shadow-lg m-5 max-h-[100vh - 100px] h-full"></div>
+    <div className="p-4 pt-5">
+      <h1 className="font-bold text-[1.25rem] mb-4">Tìm kiếm bạn bè</h1>
+      <div className="search">
+        <InputGroup className="bg-white h-10">
+          <InputGroupInput placeholder="Nhập tên bạn bè..."/>
+          <InputGroupAddon>
+            <Search/>
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+      <div className="result mt-5">
+        <h2 className="font-medium">Kết quả tìm kiếm <span className="text-slate-400 text-sm font-normal">999 kết quả</span></h2>
+        <div className="mt-5 flex flex-col gap-y-4">
+          <UserCard></UserCard>
+          <UserCard></UserCard>
+          <UserCard></UserCard>
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ src/app/(main)/friend/search/user-card/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button';
+import { UserRoundPlus } from 'lucide-react';
+import React from 'react';
+
+const UserCard = () => {
+    return (
+        <div className="flex items-center justify-between bg-white shadow-md rounded-lg p-5">
+            <div className="flex items-center gap-x-3">
+              <img src="/images/default-profile.jpg" alt="" className="w-[60px] h-[60px] object-cover rounded-full"/>
+              <div>
+                <p className="font-medium text-lg">Nguyễn Văn A</p>
+                <span className="text-sm font-thin">5 bạn chung</span>
+              </div>
+            </div>
+            <div>
+              <Button><UserRoundPlus />Kết bạn</Button>
+            </div>
+          </div>
+    );
+};
+
+export default UserCard;
